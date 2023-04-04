f38d8f85d0cf78e401fb2d457e08aa28fbdb34b8
Benjamin holden <benjamin.f.holden@outlook.com>
2023-04-03 18:22:54 -0600

    Add solutions to dollar-store-vending-machine

--- dollar-store-vending-machine/index.js
+++ dollar-store-vending-machine/index.js
@@ -1,2 +1,33 @@
+function createItemStock(name, quantity, price){
+    itemStock = {
+        name: name || 'unknown',
+        quantity: quantity || 0,
+        price: price || 1.00
+    }
+    return itemStock
+}
 
-module.exports = {}
+function makePurchase(selectedItem, moneyForPurchase){
+    if (selectedItem.price > moneyForPurchase){
+        return `Sorry, you need at least $${selectedItem.price} to make that purchase`
+    } else if (selectedItem.quantity < 1){
+        return 'Sorry, there are none left'
+    } else 
+    return `Here are your ${selectedItem.name}`
+}
+
+function collectChange(coins){
+var Total = 0
+const initialValue = 0;
+const sumWithInitial = coins.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue
+)
+return sumWithInitial
+}
+
+module.exports = {
+    createItemStock, 
+    collectChange,
+    makePurchase
+}
