296c8d1bcc8ebf3d8fb47bfb6d5746496497622e
Benjamin holden <benjamin.f.holden@outlook.com>
2023-04-05 08:56:30 -0600

    Add solutions to birthdays

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,23 @@
+function createBirthday(name, month, day){
+var birthday = {
+    name: name,
+    month: month,
+    day: day
+}
+    return birthday
+}
 
+function celebrateBirthday(birthday){
+    return `Today is ${birthday.month}/${birthday.day}! Happy birthday, ${birthday.name}!`
+}
 
-module.exports = {  };
+function countBirthdays(birthdays, givenMonth){
+    var monthTotal = [];
+    for (var i = 0; i < birthdays.length; i++){
+    if (birthdays[i].month === givenMonth){
+        monthTotal.push(birthdays[i])
+    }
+}
+return monthTotal.length
+}
+module.exports = { createBirthday, celebrateBirthday, countBirthdays };
