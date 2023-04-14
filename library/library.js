c263e6dedcc28d242b718d8e101e2aebd29890ec
Benjamin holden <benjamin.f.holden@outlook.com>
2023-04-14 08:11:10 -0600

    Add solutions to library.js

--- library/library.js
+++ library/library.js
@@ -1,3 +1,28 @@
+function issueCard(name, age){
+    var card = {
+        name: name,
+        age: age,
+        numBooksCheckedOut: 0,
+        isChild: age<12
+    }
+    return card
+}
 
+function searchByAuthor(collection,name){
+var authorResults = []
+    for (var i = 0; i < collection.length; i++){
+        if (collection[i].author === name){
+        authorResults.push(collection[i])
+    } 
+}
+console.log(authorResults.length === 0)
+if (authorResults.length === 0) {
+    return 'No book found for search criteria'
+}
+    return authorResults
+}
 
-module.exports = { };
+module.exports = { 
+    issueCard, 
+    searchByAuthor
+};
