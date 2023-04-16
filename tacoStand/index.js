508d1ecea24b6088ae0fca4baab783f7cba6cbc2
Benjamin holden <benjamin.f.holden@outlook.com>
2023-04-16 12:00:25 -0600

    Add solutions to tacostand

--- tacoStand/index.js
+++ tacoStand/index.js
@@ -1,2 +1,35 @@
+function createIngredient(name, price){
+    var ingredient = {
+        name: name || 'unknown',
+        price: price || 0.00,
+    }
+    return ingredient
+}
 
-module.exports = {}
+function createTaco(name, ingredients){
+    var taco = {
+        name: name || 'custom',
+        ingredients: ingredients || [],
+    }
+    return taco
+}
+
+function addIngredientToTaco(taco, ingredient) {
+    taco.ingredients.push(ingredient)
+    return taco
+}
+
+function calculatePrice(taco) {
+    price = 0
+    for (var i = 0; i < taco.ingredients.length; i++){
+        price += taco.ingredients[i].price
+    }
+    return price
+}
+
+module.exports = {
+    createIngredient,
+    createTaco,
+    addIngredientToTaco,
+    calculatePrice 
+}
