5adb6f1a980dcd4c7efe832e4a9cf9aa05c34d2a
Benjamin holden <benjamin.f.holden@outlook.com>
2023-04-18 10:50:18 -0600

    Add spotify solutions

--- spotify/collection.js
+++ spotify/collection.js
@@ -1,4 +1,57 @@
+function createCollection(name, description){
+  collection = {
+    name: name,
+    description: description,
+    tracks: [],
+  }
+  return collection
+}
 
+function createTrack(title,artist,duration){
+  track = {
+    title: title || 'unknown',
+    artist: artist || 'unknown',
+    duration: duration || 0,
+  }
+  return track
+}
 
+function reviewTrack(track){
+  if (track.artist === 'Red Hot Chili Peppers'){
+    return `The song ${track.title} rules!`
+  }
+  return 'I wish this was a Red Hot Chili Peppers song.'
+}
 
-  module.exports = {  }
+function addTrack(collection, track){
+collection.tracks.push(track)
+return collection
+}
+
+function getTotalDuration(collection){
+  console.log(collection.tracks)
+ var total = 0
+ for (var i = 0; i<collection.tracks.length; i++){
+   total += collection.tracks[i].duration
+ }
+  return total
+}
+
+function findTracksByArtist(collection, artist){
+  var tracksByArtist = []
+  for (var i = 0; i < collection.tracks.length; i++){
+  if(collection.tracks[i].artist === artist){
+     tracksByArtist.push(collection.tracks[i])
+  }
+}
+return tracksByArtist
+}
+
+  module.exports = { 
+    createCollection,
+    createTrack,
+    reviewTrack,
+    addTrack,
+    getTotalDuration,
+    findTracksByArtist
+   }
