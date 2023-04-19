9bd95d69a479e61b7294b66957b8a1838a01f89a
Benjamin holden <benjamin.f.holden@outlook.com>
2023-04-19 09:44:44 -0600

    Add Solutions to spa

--- spa/customer.js
+++ spa/customer.js
@@ -1,2 +1,49 @@
+function createCustomer(name, bill, bookings){
+  var customer = {
+    name: name,
+    bill: bill,
+    bookings: bookings || [],
+  }
+  return customer
+}
 
-module.exports = {  }
+function greeting(customer){
+  if (customer.bill>0){
+    return `${customer.name}! Welcome back to Happy Spa`
+  }
+  return `${customer.name}! Welcome to Happy Spa`
+}
+
+function createService(name, cost){
+  service = {
+   name: name,
+   cost: cost
+  }
+  if (!name && !cost){
+    return `Please provide service name and cost.`
+  }
+  return service
+}
+
+function bookServices(customer, service){
+  customer.bookings.push(service.name)
+  customer.bill += service.cost
+  return customer
+}
+
+function applyGiftCard(allServices, price){
+  services = []
+  for (var i = 0; i < allServices.length; i++){
+    if (allServices[i].price<=price){
+      services.push(allServices[i].name)
+    }
+  }
+  return services
+}
+module.exports = {
+  createCustomer, 
+  greeting, 
+  bookServices, 
+  createService,
+  applyGiftCard
+  }
